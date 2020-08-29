ed28c1f77312f4d1adbcd22bc9f26dee54de8430
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-08-29 16:47:10 -0400

    fibonacci sequence added

--- questions.js
+++ questions.js
@@ -7,7 +7,7 @@ console.log("Hello, World! :)")
 //------------------------------------------------------------------------//
 
 //Fizzbuzz
-//There are many ways to do this, but this is my go-to solution
+//There are many ways to do this, but this is my go-to solution. Easy to read and it gives you lots of wiggle room if the interview asks you to make it more performant.
 
 function fizzbuzz(num) {
     for (i = 0; i < num; i++) {
@@ -23,15 +23,41 @@ function fizzbuzz(num) {
     }
 };
 
-fizzbuzz(100)
+// fizzbuzz(100)
 
 //A more concise version of Fizzbuzz if you wanna go big brain
+
 // function fizzbuzzYeet(num){
 // for(let i=0;i<num;)console.log((++i%3?'':'fizz')+(i%5?'':'buzz')||i)
 // }
-
 // fizzbuzzYeet(100)
 
 //------------------------------------------------------------------------//
 //------------------------------------------------------------------------//
 //------------------------------------------------------------------------//
+
+//Fibonacci Sequence
+
+function spiralOut() {
+    let a = 0,
+        b = 1,
+        result;
+    for (i = 0; i < 10; i++) {
+        result = a + b
+        console.log(result)
+        a = b
+        b = result
+    };
+};
+
+spiralOut()
+
+//------------------------------------------------------------------------//
+//------------------------------------------------------------------------//
+//------------------------------------------------------------------------//
+
+//Sorting an array
+
+let arr = [4,1,5,3,6,12,66,123,2,77]
+
+console.log(arr.sort((a, b) => (a - b)))
